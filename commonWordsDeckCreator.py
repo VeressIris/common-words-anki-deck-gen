1eb2789c7b656dd1db2115b65d84dfcfc8182f4b
VeressIris <irisveress@gmail.com>
2024-07-06 15:57:20 +0300

    added web scraping to dynamically get the link to the excel file & more
    made the url dynamic so that the user only has to specify the language instead of the entire link & limited number of common words so that it can be specified by the user

--- commonWordsDeckCreator.py
+++ commonWordsDeckCreator.py
@@ -1,15 +1,31 @@
 import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+from itertools import islice
+
+language = "german"
+
+# scraping the page 101languages.net page
+url = f"https://www.101languages.net/{language}/most-common-{language}-words/"
+
+headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
+page = requests.get(url, headers=headers).text
+
+doc = BeautifulSoup(page, 'html.parser')
+
+links = doc.find_all('a')
 
 # url of google sheet present on 101languages.net
-raw_url = "https://docs.google.com/spreadsheets/d/1VXTLrhgZ-HOOy65m7H76cPnm3z1jobbjp54CBs8nK2A/edit?gid=449211973#gid=449211973"
+raw_excel_url = next((link.get('href') for link in links if '2,000' in link.text), "")
+print(raw_excel_url)
 
 # converting the google sheet to csv so that pandas can read it
-csv_url = raw_url.rsplit('edit')[0] + 'export?format=csv'
+csv_url = raw_excel_url.rsplit('edit')[0] + 'export?format=csv'
 
 df = pd.read_csv(csv_url)
-target_language = df.columns[1]
-list_of_words = df[target_language].tolist()
+list_of_words = df[language.capitalize()].tolist()
 
+num_of_words = 15 # number of most common words wanted
 with open('mostCommonWords.txt', 'w', encoding='utf-8') as file:
-    for word in list_of_words:
+    for word in islice(list_of_words, num_of_words):
         file.write(word + '\n')
